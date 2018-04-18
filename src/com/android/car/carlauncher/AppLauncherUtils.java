28a9c9847207bc04f0560ff0948a04d9c7babcdc
Adora Zhang <qiaozhang@google.com>
2018-04-18 11:38:20 -0700

    Cherry pick ag/4027086: Make non-DO apps in app selector greyed-out and non-launchable. Also fix the inconsistent names in recent app row.
    Bug: 79208753
    Bug: 78121348
    Test: Manual
    Change-Id: Ib88f3f42f54bb79a934560a5ad04b6483807eeec

--- src/com/android/car/carlauncher/AppLauncherUtils.java
+++ src/com/android/car/carlauncher/AppLauncherUtils.java
@@ -16,15 +16,50 @@
 
 package com.android.car.carlauncher;
 
+import android.annotation.Nullable;
+import android.car.CarNotConnectedException;
+import android.car.content.pm.CarPackageManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.LauncherActivityInfo;
+import android.content.pm.LauncherApps;
+import android.content.pm.PackageManager;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.drawable.Drawable;
+import android.os.Process;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * Util class that contains helper method used by app launcher classes.
  */
 class AppLauncherUtils {
 
-    private AppLauncherUtils() {}
+    private static final String TAG = "AppLauncherUtils";
+
+    private AppLauncherUtils() {
+    }
+
+    /**
+     * Comparator for {@link AppMetaData} that sorts the list
+     * first by is distraction optimized in descending order (optimized apps first)
+     * then by the "displayName" property in ascending order.
+     * Used when distraction optimization is required.
+     */
+    static final Comparator<AppMetaData> DISTRACTION_OPTIMIZED_COMPARATOR = Comparator
+            .comparing(AppMetaData::getIsDistractionOptimized).reversed()
+            .thenComparing(AppMetaData::getDisplayName, String::compareToIgnoreCase);
+
+    /**
+     * Comparator for {@link AppMetaData} that sorts the list
+     * by the "displayName" property in ascending order.
+     */
+    static final Comparator<AppMetaData> ALPHABETICAL_COMPARATOR = Comparator
+            .comparing(AppMetaData::getDisplayName, String::compareToIgnoreCase);
 
     /**
      * Helper method that launches the app given the app's AppMetaData.
@@ -35,5 +70,84 @@ class AppLauncherUtils {
         Intent intent =
                 context.getPackageManager().getLaunchIntentForPackage(app.getPackageName());
         context.startActivity(intent);
+    }
+
+    /**
+     * Converts a {@link Drawable} to grey scale.
+     *
+     * @param drawable the original drawable
+     * @return the grey scale drawable
+     */
+    static Drawable toGrayscale(Drawable drawable) {
+        ColorMatrix matrix = new ColorMatrix();
+        matrix.setSaturation(0);
+        ColorMatrixColorFilter filter = new ColorMatrixColorFilter(matrix);
+        // deep copy the original drawable
+        Drawable newDrawable = drawable.getConstantState().newDrawable().mutate();
+        newDrawable.setColorFilter(filter);
+        return newDrawable;
+    }
+
+    /**
+     * Gets all apps that support launching from launcher in unsorted order.
+     *
+     * @param launcherApps      The {@link LauncherApps} system service
+     * @param carPackageManager The {@link CarPackageManager} system service
+     * @param packageManager    The {@link PackageManager} system service
+     * @return a list of all apps' metadata
+     */
+    @Nullable
+    static List<AppMetaData> getAllLaunchableApps(
+            LauncherApps launcherApps,
+            CarPackageManager carPackageManager,
+            PackageManager packageManager) {
+
+        if (launcherApps == null || carPackageManager == null || packageManager == null) {
+            return null;
+        }
+
+        List<AppMetaData> apps = new ArrayList<>();
+
+        Intent intent = new Intent(Intent.ACTION_MAIN, null);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        List<LauncherActivityInfo> availableActivities =
+                launcherApps.getActivityList(null, Process.myUserHandle());
+        for (LauncherActivityInfo info : availableActivities) {
+            String packageName = info.getComponentName().getPackageName();
+            boolean isDistractionOptimized =
+                    isActivityDistractionOptimized(carPackageManager, packageName, info.getName());
+
+            AppMetaData app = new AppMetaData(
+                    info.getLabel(),
+                    packageName,
+                    info.getApplicationInfo().loadIcon(packageManager),
+                    isDistractionOptimized);
+            apps.add(app);
+        }
+        return apps;
+    }
+
+    /**
+     * Gets if an activity is distraction optimized.
+     *
+     * @param carPackageManager The {@link CarPackageManager} system service
+     * @param packageName       The package name of the app
+     * @param activityName      The requested activity name
+     * @return true if the supplied activity is distraction optimized
+     */
+    static boolean isActivityDistractionOptimized(
+            CarPackageManager carPackageManager, String packageName, String activityName) {
+        boolean isDistractionOptimized = false;
+        // try getting distraction optimization info
+        try {
+            if (carPackageManager != null) {
+                isDistractionOptimized =
+                        carPackageManager.isActivityDistractionOptimized(packageName, activityName);
+            }
+        } catch (CarNotConnectedException e) {
+            Log.e(TAG, "Car not connected when getting DO info", e);
+        }
+        return isDistractionOptimized;
     }
 }
